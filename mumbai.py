4a693ec6d46b792ae24ebf9c611bcda92685f4a9
Kaustuv Chatterjee <chatterjee.kaustuv@gmail.com>
2020-12-21 08:55:16 +0530

    mumbai

--- mumbai.py
+++ mumbai.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 from scipy.signal import savgol_filter
 import streamlit as st
 
-
+@st.cache
 def load_data():
     url = 'https://api.covid19india.org/csv/latest/districts.csv'
     data = pd.read_csv(url)
