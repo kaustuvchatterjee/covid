07e3be5601fdccc3e3f96ecebd8c7e7bac4deb7a
Kaustuv <chatterjee.kaustuv@gmail.com>
2021-01-29 10:40:28 +0530

    updated mumbai.py

---
+++ .virtual_documents/kite_tutorial.ipynb.py
@@ -0,0 +1,22 @@
+# Run me!
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# Put code in me
+x = np.linspace(-np.pi,np.pi,50)
+y = np.sin(x)
+plt.plot(x,y)
+
+
+
+# Run me!
+url = 'https://kite.com/kite-public/iris.csv'
+df = pd.read_csv(url)
+df.head()
+
+
+# Put code in me
+
+
