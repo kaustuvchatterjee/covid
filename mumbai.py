21685a228e81d0c60c8f33178bd131087756430b
Kaustuv <chatterjee.kaustuv@gmail.com>
2021-02-19 08:42:48 +0530

    Update

--- mumbai.py
+++ mumbai.py
@@ -11,6 +11,7 @@ import plotly.graph_objects as go
 from scipy.signal import savgol_filter
 
 
+
 def load_data():
     url = 'https://api.covid19india.org/csv/latest/districts.csv'
     data = pd.read_csv(url)
